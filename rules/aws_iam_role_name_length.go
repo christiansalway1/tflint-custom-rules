d47fb2fe5860ac3708b92659ac954a60caa69200
Christian Salway <christian@salway.im>
2022-11-14 14:45:04 +0000

    rule to limit iam role name length

---
+++ rules/aws_iam_role_name_length.go
@@ -0,0 +1,106 @@
+package rules
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/terraform-linters/tflint-plugin-sdk/hclext"
+	"github.com/terraform-linters/tflint-plugin-sdk/logger"
+	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
+)
+
+// AwsIAMRoleInvalidNameRule2 checks the pattern is valid
+type AwsIAMRoleInvalidNameRule2 struct {
+	tflint.DefaultRule
+
+	resourceType  string
+	attributeName string
+	max           int
+	min           int
+	pattern       *regexp.Regexp
+}
+
+// NewAwsIAMRoleInvalidNameRule2 returns new rule with default attributes
+func NewAwsIAMRoleInvalidNameRule2() *AwsIAMRoleInvalidNameRule2 {
+	return &AwsIAMRoleInvalidNameRule2{
+		resourceType:  "aws_iam_role",
+		attributeName: "name",
+		max:           20,
+		min:           1,
+		pattern:       regexp.MustCompile(`^[\w+=,.@-]+$`),
+	}
+}
+
+// Name returns the rule name
+func (r *AwsIAMRoleInvalidNameRule2) Name() string {
+	return "aws_iam_role_invalid_name2"
+}
+
+// Enabled returns whether the rule is enabled by default
+func (r *AwsIAMRoleInvalidNameRule2) Enabled() bool {
+	return true
+}
+
+// Severity returns the rule severity
+func (r *AwsIAMRoleInvalidNameRule2) Severity() tflint.Severity {
+	return tflint.ERROR
+}
+
+// Link returns the rule reference link
+func (r *AwsIAMRoleInvalidNameRule2) Link() string {
+	return ""
+}
+
+// Check checks the pattern is valid
+func (r *AwsIAMRoleInvalidNameRule2) Check(runner tflint.Runner) error {
+	logger.Trace("Check `%s` rule", r.Name())
+
+	resources, err := runner.GetResourceContent(r.resourceType, &hclext.BodySchema{
+		Attributes: []hclext.AttributeSchema{
+			{Name: r.attributeName},
+		},
+	}, nil)
+	if err != nil {
+		return err
+	}
+
+	for _, resource := range resources.Blocks {
+		attribute, exists := resource.Body.Attributes[r.attributeName]
+		if !exists {
+			continue
+		}
+
+		var val string
+		err := runner.EvaluateExpr(attribute.Expr, &val, nil)
+
+		err = runner.EnsureNoError(err, func() error {
+			if len(val) > r.max {
+				runner.EmitIssue(
+					r,
+					"name must be 20 characters or less",
+					attribute.Expr.Range(),
+				)
+			}
+			if len(val) < r.min {
+				runner.EmitIssue(
+					r,
+					"name must be 1 characters or higher",
+					attribute.Expr.Range(),
+				)
+			}
+			if !r.pattern.MatchString(val) {
+				runner.EmitIssue(
+					r,
+					fmt.Sprintf(`"%s" does not match valid pattern %s`, truncateLongMessage(val), `^[\w+=,.@-]+$`),
+					attribute.Expr.Range(),
+				)
+			}
+			return nil
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
